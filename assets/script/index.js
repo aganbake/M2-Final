f3829faa48f538802797dcd87854a3e7476d4f55
aganbake <abdul.villarroel@gmail.com>
2023-04-15 12:50:21 -0400

    final-push

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://digimon-api.vercel.app/api/digimon";
 var digimon;
-var digi = 0;
+let digi = 0;
 
 function apiCall(number) {
   fetch(BASE_URL)
@@ -37,13 +37,16 @@ function digimonList() {
     .catch((error) => console.log(error));
 
   function createDigimon(data) {
+    let table = document.getElementById("tbody");
+    table.innerHTML = "";
+    let digi = 0;
     for (let digimon of data) {
       let tabla = document.createElement("tr");
       tabla.innerHTML = `
       <td>${digi}</td>
       <td>${digimon.name}</td>
       <td>${digimon.level}</td>
-      <td><img width="15%" src="${digimon.img}"></td>
+      <td><img class="digimon-table" width="15%" src="${digimon.img}"></td>
     `;
       $("#tbody").append(tabla);
       digi++;
@@ -52,33 +55,45 @@ function digimonList() {
 }
 
 function search() {
-  let search = document.getElementById("customSearch").value;
-  search = search.toLowerCase();
-  console.log(search);
-  fetch(BASE_URL + "/name:" + search)
+  let digi = 0;
+  let name = document.getElementById("needDigimon").value;
+  fetch(BASE_URL + "/name/" + name)
     .then((results) => results.json())
     .then((data) => {
-      let div = document.createElement("div");
-      div.innerHTML = `
-      <h1 class="big-heading">${data[0].name}</h1>
-      <img class="digimon-img" src=${data[0].img}>
-      <h3>Nivel ${data[0].level}</h3>`;
-      $("Digimon-Searched").append(div);
+      console.log(data);
+      createDigimon(data);
     })
     .catch((error) => console.log(error));
-  // buscarDigimon(search);
+
+  function createDigimon(data) {
+    let tabla = document.createElement("tr");
+    tabla.innerHTML = `
+      <td>${digi}</td>
+      <td>${data[0].name}</td>
+      <td>${data[0].level}</td>
+      <td><img class="digimon-table" width="15%" src="${data[0].img}"></td>
+    `;
+    let table = document.getElementById("tbody");
+    table.innerHTML = "";
+    $("#tbody").append(tabla);
+  }
 }
 
-function buscarDigimon(nombre) {
-  fetch(BASE_URL + "/name:" + nombre)
-    .then((results) => results.json())
-    .then((data) => {
-      let div = document.createElement("div");
-      div.innerHTML = `
-      <h1 class="big-heading">${data[0].name}</h1>
-      <img class="digimon-img" src=${data[0].img}>
-      <h3>Nivel ${data[0].level}</h3>`;
-      $("Digimon-Searched").append(div);
-    })
-    .catch((error) => console.log(error));
-}
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1000) {
+      $("#btn-back-to-top").fadeIn();
+    } else {
+      $("#btn-back-to-top").fadeOut();
+    }
+  });
+  $("#btn-back-to-top").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
+});
